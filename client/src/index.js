6f3c396bf12d2419260783cfa443250bce8ad8c5
TimaVol <tima.voloshuk@gmail.com>
2022-10-14 19:46:50 +0300

    install necessary pkgs

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/globals.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );

d156e9e6787de4f1b5384b76d2b2662c5aa52ea4
TimaVol <tima.voloshuk@gmail.com>
2022-10-18 21:41:36 +0300

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+
 import logo from "./logo.svg";
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ client/src/common/atoms.js
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+export const tickersState = atom({
+    key: 'tickers',
+    default: [],
+})
+

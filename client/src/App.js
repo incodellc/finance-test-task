b44f0a9c53a96a3ff4952df3db04348ff921e713
Mikita Havryliuk <chinter.games1@gmail.com>
2023-07-18 17:08:03 +0300

    added tickers-store

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTickers } from './redux/tickers-store';
+
 function App() {
+  const dispatch = useDispatch();
+  
+  console.log(dispatch);
   return (
     <h1>Hello world!</h1>
   );

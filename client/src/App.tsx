4f8a32b68d4faa6e4613f08f4a37d2eeef8ff52c
Mykyta Kasianenko <mykytakasianenko@gmail.com>
2024-02-14 12:24:52 +0200

    feat: theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
-import { Button } from "./components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function App() {
   return (
-    <div>
-      <Button>Click</Button>
-    </div>
-  )
+    <>
+      <div className="mt-6">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="account">Catalog</TabsTrigger>
+            <TabsTrigger value="password">Saved</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from '@/state/store'
+import { ThemeProvider } from '@/providers/theme/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+    </ThemeProvider>
     </Provider>
   </React.StrictMode>,
 )
